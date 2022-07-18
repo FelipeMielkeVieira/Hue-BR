cec27026c0e90507f6e51aba151be68e3023319f
larissa71 <felipe_mielke-vieira@estudante.sc.senai.br>
2022-07-18 09:50:48 -0300

    18/07 - 09:50

--- public/niveis/tutorial.js
+++ public/niveis/tutorial.js
@@ -1,13 +1,25 @@
+let totalWidth = window.innerWidth * 0.98;
+let totalHeight = window.innerHeight * 0.96;
+console.log(totalHeight)
+
 kaboom({
-    width: 320,
-    height: 240,
+    debug: true,
     font: "sinko",
-    background: [ 0, 0, 255, ],
+    width: totalWidth,
+    height: totalHeight,
+    background: [ 0, 255, 255, ],
 })
 
-loadSprite("player", "sprites/player.png")
+loadSprite("player", "sprites/playerR.png")
 
-add([
+const player = add([
     sprite("player"),
-    pos(80, 40),
+    pos(80, 550),
+    area()
 ])
+
+let moveSpeed = 120;
+
+onKeyDown("up", () => {
+    player.move(UP, moveSpeed);
+})
