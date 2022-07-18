8069a71397007c79adb6f2bf83e2fa1f0a624554
larissa71 <felipe_mielke-vieira@estudante.sc.senai.br>
2022-07-18 08:31:38 -0300

    18/07 - 08:31
    Criação do servidor e rotas básicas com express e nodemon.

---
+++ app.js
@@ -0,0 +1,33 @@
+// create an express app
+const express = require("express")
+const app = express()
+
+const path = require("path");
+
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/login.html"));
+})
+
+app.get("/cadastro", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/cadastro.html"));
+})
+
+app.get("/menu", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/menu.html"));
+})
+
+app.get("/campanhas", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/campanhas.html"));
+})
+
+app.get("/niveis/1", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/niveis/tutorial.html"));
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
