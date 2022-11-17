ba30d026efcaf0f9ccac37b58dd4ccd0f96e5b16
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2022-11-17 12:32:19 -0300

    Iniciando nível 2

--- app.js
+++ app.js
@@ -29,5 +29,4 @@ app.get("/niveis/1", function (req, res) {
 })
 
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running at port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running at port 3000"));
--- public/niveis/jogo.js
+++ public/niveis/jogo.js
@@ -1,6 +1,7 @@
 let totalWidth = window.innerWidth * 0.98;
 let totalHeight = window.innerHeight * 0.96;
 let temChave = false
+let scaleBut = 2
 
 kaboom({
     debug: true,
@@ -38,7 +39,7 @@ loadSprite("blackWall", "sprites/walls/blackWall.png")
 loadSprite("redWall", "sprites/walls/redWall.png")
 loadSprite("orangeWall", "sprites/walls/orangeWall.png")
 
-scene("tutorial", () => {
+scene("nivel1", () => {
 
     const background = add([
         pos(0, 0),
@@ -46,6 +47,14 @@ scene("tutorial", () => {
         outline(4),
         color(255, 255, 255),
         area(),
+    ])
+
+    add([
+        pos(20, 20),
+        color(0, 0, 0, 0),
+        text("Nivel 1", {
+            size: 35,
+        }),
     ])
 
     add([
@@ -205,7 +214,7 @@ scene("tutorial", () => {
     })
 
     player.onCollide("levelEnd", () => {
-        go("level2")
+        go("nivel2")
     })
 
     let speed = 200;
@@ -284,12 +293,317 @@ scene("tutorial", () => {
 
 });
 
-scene("level2", () => {
+scene("nivel2", () => {
+    const background = add([
+        pos(0, 0),
+        rect(totalWidth, totalHeight),
+        outline(4),
+        color(255, 255, 255),
+        area(),
+    ])
+
     add([
-        text("Em desenvolvimento"),
-        pos(totalWidth*0.3, totalHeight*0.4),
-        scale(3)
+        pos(20, 20),
+        color(125, 125, 125),
+        text("Nivel 2", {
+            size: 35,
+        }),
+    ])
+
+    add([
+        "greenBut",
+        sprite("greenButL"),
+        pos(totalWidth * 0.056, totalHeight * 0.65),
+        scale(scaleBut),
+        area(),
+    ])
+
+    add([
+        "redBut",
+        sprite("redButL"),
+        pos(totalWidth * 0.056, totalHeight * 0.1),
+        scale(scaleBut),
+        area(),
+    ])
+
+    add([
+        "redBut",
+        sprite("redButR"),
+        pos(totalWidth * 0.056, totalHeight * 0.3),
+        scale(scaleBut),
+        area(),
+    ])
+
+    add([
+        "orangeBut",
+        sprite("orangeButL"),
+        pos(totalWidth * 0.056, totalHeight * 0.5),
+        scale(scaleBut),
+        area(),
+    ])
+
+    add([
+        "whiteBut",
+        sprite("whiteButR"),
+        pos(totalWidth * 0.08, totalHeight * 0.65),
+        scale(scaleBut),
+        area(),
+    ])
+
+    add([
+        "blueBut",
+        sprite("blueButL"),
+        pos(totalWidth * 0.47, totalHeight * 0.8),
+        scale(scaleBut),
+        area(),
+    ])
+
+    add([
+        "greenBut",
+        sprite("greenButR"),
+        pos(totalWidth * 0.5, totalHeight * 0.8),
+        scale(scaleBut),
+        area(),
+    ])
+
+    add([
+        "yellowBut",
+        sprite("yellowButL"),
+        pos(totalWidth * 0.85, totalHeight * 0.2),
+        scale(scaleBut),
+        area(),
+    ])
+
+    add([
+        "blueBut",
+        sprite("blueButR"),
+        pos(totalWidth * 0.88, totalHeight * 0.2),
+        scale(scaleBut),
+        area(),
+    ])
+
+    const greenWall = add([
+        "greenWall",
+        rect(70, totalHeight - 20),
+        color(0, 255, 0),
+        pos(totalWidth * 0.2, 10),
+        state("visivel", ["visivel", "invisivel"]),
+        area(),
+        solid(),
+    ])
+
+    const orangeWall = add([
+        "orangeWall",
+        rect(30, 105),
+        color(249, 157, 49),
+        pos(totalWidth * 0.89, totalHeight * 0.818),
+        state("visivel", ["visivel", "invisivel"]),
+        area(),
+        solid(),
+    ])
+
+    const redWall = add([
+        "redWall",
+        rect(30, 105),  
+        color(255, 0, 0),
+        pos(totalWidth * 0.8, totalHeight * 0.818),
+        state("visivel", ["visivel", "invisivel"]),
+        area(),
+        solid(),
+    ])
+
+    const blueWall = add([
+        "blueWall",
+        rect(70, (totalHeight * 0.3) - 10),
+        pos(totalWidth * 0.7, 10),
+        color(0, 0, 255),
+        area(),
+        solid(),
+    ])
+
+    const yellowWall = add([
+        "yellowWall",
+        rect(70, (totalHeight * 0.7) - 10),
+        pos(totalWidth * 0.7, totalHeight * 0.3),
+        color(247, 217, 23),
+        area(),
+        solid(),
+    ])
+
+    add([
+        "whiteWall",
+        rect(30, 140),
+        pos(totalWidth * 0.12, totalHeight * 0.5963),
+        color(128, 128, 128),
+        area(),
+        solid(),
+    ])
+
+    add([
+        "whiteWall",
+        rect(170, 30),
+        pos(totalWidth * 0, totalHeight * 0.77),
+        color(128, 128, 128),
+        area(),
+        solid(),
+    ])
+
+    add([
+        "whiteWall",
+        rect(170, 30),
+        pos(totalWidth * 0.89, totalHeight * 0.77),
+        color(128, 128, 128),
+        area(),
+        solid(),
+    ])
+
+    const player = add([
+        "player",
+        sprite("playerU"),
+        pos(totalWidth * 0.05, totalHeight * 0.85),
+        area(),
+        solid(),
+    ])
+
+    player.onCollide("greenBut", () => {
+        background.use(color(0, 255, 0))
+        destroy(greenWall)
+        readd(blueWall)
+        readd(orangeWall)
+        readd(redWall)
+        readd(yellowWall)
+    })
+
+    player.onCollide("whiteBut", () => {
+        background.use(color(255, 255, 255))
+        readd(greenWall)
+        readd(blueWall)
+        readd(yellowWall)
+        readd(orangeWall)
+        readd(redWall)
+    })
+
+    player.onCollide("blueBut", () => {
+        background.use(color(0, 0, 255))
+        destroy(blueWall)
+        readd(greenWall)
+        readd(yellowWall)
+        readd(orangeWall)
+        readd(redWall)
+    })
+
+    player.onCollide("yellowBut", () => {
+        background.use(color(247, 217, 23))
+        destroy(yellowWall)
+        readd(greenWall)
+        readd(blueWall)
+        readd(orangeWall)
+        readd(redWall)
+    })
+
+    player.onCollide("orangeBut", () => {
+        background.use(color(249, 157, 49))
+        destroy(orangeWall)
+        readd(greenWall)
+        readd(blueWall)
+        readd(yellowWall)
+        readd(redWall)
+    })
+
+    player.onCollide("redBut", () => {
+        background.use(color(255, 0, 0))
+        destroy(redWall)
+        readd(greenWall)
+        readd(blueWall)
+        readd(yellowWall)
+        readd(orangeWall)
+    })
+
+    add([
+        "levelEnd",
+        sprite("levelEnd"),
+        pos(totalWidth * 0.93, totalHeight * 0.85),
+        scale(1.5),
+        area(),
+    ])
+
+    player.onCollide("levelEnd", () => {
+        go("level2")
+    })
+
+    let speed = 200;
+
+    onKeyDown("left", () => {
+        player.use(sprite("playerL"));
+        player.move(-speed, 0);
+    })
+    onKeyDown("a", () => {
+        player.use(sprite("playerL"));
+        player.move(-speed, 0);
+    })
+
+    onKeyDown("right", () => {
+        player.use(sprite("playerR"));
+        player.move(speed, 0);
+    })
+    onKeyDown("d", () => {
+        player.use(sprite("playerR"));
+        player.move(speed, 0);
+    })
+
+    onKeyDown("up", () => {
+        player.use(sprite("playerU"));
+        player.move(0, -speed);
+    })
+    onKeyDown("w", () => {
+        player.use(sprite("playerU"));
+        player.move(0, -speed);
+    })
+
+    onKeyDown("down", () => {
+        player.use(sprite("playerD"));
+        player.move(0, speed);
+    })
+    onKeyDown("s", () => {
+        player.use(sprite("playerD"));
+        player.move(0, speed);
+    })
+
+    const borderTop = add([
+        "borderTop",
+        rect(totalWidth, 10),
+        color(0, 0, 0, 0),
+        pos(0, 0),
+        area(),
+        solid(),
+    ])
+
+    const borderLeft = add([
+        "borderLeft",
+        rect(10, totalHeight),
+        color(0, 0, 0, 0),
+        pos(0, 0),
+        area(),
+        solid(),
+    ])
+
+    const borderBottom = add([
+        "borderBottom",
+        rect(totalWidth, 10),
+        color(0, 0, 0, 0),
+        pos(0, totalHeight - 10),
+        area(),
+        solid(),
+    ])
+
+    const borderRight = add([
+        "borderRight",
+        rect(10, totalHeight),
+        color(0, 0, 0, 0),
+        pos(totalWidth - 10, 0),
+        area(),
+        solid(),
     ])
 })
 
-go("tutorial");
+go("nivel2");
